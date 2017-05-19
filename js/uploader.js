11a321f24ebc4dc9599964bff9ea64c7201d7f7d
mraballard <mraballard@gmail.com>
2017-05-19 14:30:03 -0400

    Added bootstrap files, receiving error after selecting file to upload

--- js/uploader.js
+++ js/uploader.js
@@ -1,8 +1,47 @@
 
-;(function () {
+(function () {
     'use strict';
 
-    angular.module('processApp')
+	function UploadController ($scope, $http) {
+		var ctrl = this;
+
+		$('#fileupload').fileupload({
+          dataType: 'json',
+          formData: {
+            access_token: '557deab0c0daafd795940ed2c0b22049df04d3e2da943225dc4c45fce313e3e4'
+          },
+          add: function (e, data) {
+            scope.hashId   = '';
+            scope.progress = 0;
+            scope.status   = 'uploading';
+            scope.url      = '';
+
+            data.submit();
+          },
+          done: function (e, data) {
+            if (data.result.hashed_id != '') {
+              scope.hashId = data.result.hashed_id;
+              scope.checkStatus();
+            }
+          },
+          progressall: function (e, data) {
+            if (data.total > 0) {
+              scope.$apply(function(){
+                scope.progress = parseInt(data.loaded / data.total * 100, 10);
+              });
+            }
+          }
+        });
+
+		console.log("inside");
+
+	}
+
+ angular.module('processApp')
+    .component('uploader', {
+  		templateUrl: 'uploader.html',
+  		controller: ['$scope', '$http', UploadController],
+	});
 
 }());
 
