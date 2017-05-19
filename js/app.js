fba80cb7faf0fe2dcb0bfc183594e3473869c291
mraballard <mraballard@gmail.com>
2017-05-19 11:05:24 -0400

    basic angular app setup

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+;(function () {
+    'use strict';
+
+    angular.module('processApp', [
+        'blueimp.fileupload'
+    ])
+
+}());
+
+console.log("app.js");
---
+++ js/uploader.js
@@ -0,0 +1,9 @@
+
+;(function () {
+    'use strict';
+
+    angular.module('processApp')
+
+}());
+
+console.log("uploader.js");
