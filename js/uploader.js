200821f81334a99894ba4626bec704a5e9d82b7e
mraballard <mraballard@gmail.com>
2017-05-19 15:46:27 -0400

    Add custom css, latest bootstrap & jquery. Fix progress bar in uploader.js using ..

--- js/uploader.js
+++ js/uploader.js
@@ -3,37 +3,36 @@
     'use strict';
 
 	function UploadController ($scope, $http) {
-		var ctrl = this;
+		var $ctrl = this;
 
 		$('#fileupload').fileupload({
-          dataType: 'json',
-          formData: {
-            access_token: '557deab0c0daafd795940ed2c0b22049df04d3e2da943225dc4c45fce313e3e4'
-          },
-          add: function (e, data) {
-            $scope.hashId   = '';
-            $scope.progress = 0;
-            $scope.status   = 'uploading';
-            $scope.url      = '';
+	        dataType: 'json',
+	        formData: {
+	     	   access_token: '557deab0c0daafd795940ed2c0b22049df04d3e2da943225dc4c45fce313e3e4'
+	        },
+	        add: function (e, data) {
+	            $scope.hashId   = '';
+	            $scope.progress = 0;
+	            $scope.status   = 'uploading';
+	            $scope.url      = 'https://upload.wistia.com';
 
-            data.submit();
-          },
-          done: function (e, data) {
-            if (data.result.hashed_id != '') {
-              $scope.hashId = data.result.hashed_id;
-              $scope.checkStatus();
-            }
-          },
-          progressall: function (e, data) {
-            if (data.total > 0) {
-              $scope.progress = parseInt(data.loaded / data.total * 100, 10);
-              };
-            }
-          }
-        );
-
-		console.log("inside");
-
+	            data.submit();
+	        },
+        	done: function (e, data) {
+            	if (data.result.hashed_id != '') {
+              		$scope.hashId = data.result.hashed_id;
+              		// $ctrl.checkStatus();
+            	}
+          	},
+	        progressall: function (e, data) {
+	        	if (data.total > 0) {
+	            	console.log($scope.progress);
+		            $scope.$apply(function() {
+		            	$scope.progress = parseInt(data.loaded / data.total * 100, 10);
+		            });
+	            };
+	        }
+	    });
 	}
 
  angular.module('processApp')
