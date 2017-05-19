be5c5873b528b6de8894a808a9af2f2217c816bf
mraballard <mraballard@gmail.com>
2017-05-19 19:59:20 -0400

    Lower timeout between calls to wistia. Add '.' to processing message to show some movement. Remove console log. Player does not play video, although it loads correctly and the player scroll works to fast forward and reverse. Wistia service may be down...?

--- js/uploader.js
+++ js/uploader.js
@@ -8,29 +8,24 @@
 		$ctrl.api_password = '080fcacb911fc78b96a1fa33ea1a6cd94640b0b36d90c6c00576b8c162125c78';
 
 		$ctrl.getMedia = function() {
-			return new Promise((resolve, reject) => {
 				let url = 'https://api.wistia.com/v1/medias/' + $ctrl.hashId + '.json?api_password=' + $ctrl.api_password;
 				$http.get(url)
 				.then((response) => {
 					let status = response.data.status;
-					if (status == 'failed') {
-						reject();			
-					} else if (status !== 'ready') {
+					if (status !== 'ready') {
 						$timeout(function(){
+							$ctrl.step += '.';
 							$ctrl.getMedia();
-						}, 3000);
+						}, 2000);
 					} else if (status == 'ready') {
 						console.log(response);
 						let mostRecent = response.data.assets.length - 1;
-						console.log("Most recent", response.data.assets[mostRecent].url);
-						$scope.url = $sce.trustAsResourceUrl(response.data.assets[mostRecent].url);
-						resolve();
+						$scope.url = $sce.trustAsResourceUrl('https://fast.wistia.net/embed/iframe/' + $ctrl.hashId + '?videoFoam=true&autoPlay=true');
 					}
 				})
 				.catch((error) => {
 					console.log(error);
 				});
-			});
 		}
 
 		$('#fileupload').fileupload({
@@ -41,7 +36,8 @@
 	        add: function (e, data) {
 	            $ctrl.hashId   = '';
 	            $scope.progress = 0;
-	            $scope.status   = 'uploading';
+	            $ctrl.step = '.';
+	            $scope.status   = 'Uploading';
 	            $scope.url      = '';
 
 	            data.submit();
@@ -49,9 +45,7 @@
         	done: function (e, data) {
             	if (data.result.hashed_id != '') {
               		$ctrl.hashId = data.result.hashed_id;
-              		$scope.status   = 'Done!';
-              		console.log($scope.hashId);
-              		console.log(data);
+              		$scope.status   = 'Processing... please wait.';
               		$ctrl.getMedia();
             	}
           	},
