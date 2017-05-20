1a2161decae80837699d40c6394ccd34cd214d73
mraballard <mraballard@gmail.com>
2017-05-20 13:58:25 -0400

    Removed unecessary files and code. Add comments to code.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # blueimp-wistia-angular-component
+## A simple angular component which uses the blueimp jQuery fileupload plugin to upload a video to Wistia and play the video within an iframe.
--- js/uploader.js
+++ js/uploader.js
@@ -2,69 +2,85 @@
 (function () {
     'use strict';
 
+    angular.module('processApp')
+    .component('uploader', {
+  		templateUrl: 'uploader.html',
+  		controller: ['$scope', '$http', '$sce', '$timeout', UploadController],
+	});
+
 	function UploadController ($scope, $http, $sce, $timeout) {
+		
 		var $ctrl = this;
 
+		// Add Wistia api_password here
 		$ctrl.api_password = '080fcacb911fc78b96a1fa33ea1a6cd94640b0b36d90c6c00576b8c162125c78';
 
+		// Function calls Wistia API to retrieve media after upload completes.
 		$ctrl.getMedia = function() {
-				let url = 'https://api.wistia.com/v1/medias/' + $ctrl.hashId + '.json?api_password=' + $ctrl.api_password;
-				$http.get(url)
-				.then((response) => {
-					let status = response.data.status;
-					if (status !== 'ready') {
-						$timeout(function(){
-							$ctrl.step += '.';
-							$ctrl.getMedia();
-						}, 2000);
-					} else if (status == 'ready') {
-						console.log(response);
-						let mostRecent = response.data.assets.length - 1;
-						$scope.url = $sce.trustAsResourceUrl('https://fast.wistia.net/embed/iframe/' + $ctrl.hashId + '?videoFoam=true&autoPlay=true');
-					}
-				})
-				.catch((error) => {
-					console.log(error);
-				});
+
+			// The following URL is defined at Wistia: https://wistia.com/doc/data-api#making_requests
+			let url = 'https://api.wistia.com/v1/medias/' + $ctrl.hashId + '.json?api_password=' + $ctrl.api_password;
+			
+			$http.get(url)
+			.then((response) => {
+				let status = response.data.status;
+				
+				// If response status of media is not ready, wait 2 seconds and repeat call
+				if (status !== 'ready') {
+					$timeout(function(){
+						$ctrl.step += '.';
+						$ctrl.getMedia();
+					}, 2000);
+				// If response status is ready, load Wistia video URL to player iframe
+				} else if (status == 'ready') {
+					let mostRecent = response.data.assets.length - 1;
+					$scope.url = $sce.trustAsResourceUrl('https://fast.wistia.net/embed/iframe/' + $ctrl.hashId + '?videoFoam=true&autoPlay=true');
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+			});
 		}
 
+		// Blueimp Jquery fileupload plugin
 		$('#fileupload').fileupload({
 	        dataType: 'json',
 	        formData: {
 	     	   api_password: $ctrl.api_password
 	        },
 	        add: function (e, data) {
-	            $ctrl.hashId   = '';
-	            $scope.progress = 0;
+	        	// This hash ID will be used to uniquely identify the project within the Wistia system
+	            $ctrl.hashId   = ''; 
+	            // Progress for progress bar in html view
+	            $ctrl.progress = 0;
+	            // Step for "Please wait" message
 	            $ctrl.step = '.';
+	            // Status message
 	            $scope.status   = 'Uploading';
+	            // Initialize url to '', hiding iframe
 	            $scope.url      = '';
 
+	            // Submit file to upload with defined formData
 	            data.submit();
 	        },
         	done: function (e, data) {
             	if (data.result.hashed_id != '') {
+            		// Save Wistia project hash ID
               		$ctrl.hashId = data.result.hashed_id;
-              		$scope.status   = 'Processing... please wait.';
+              		$scope.status   = 'Processing, please wait.';
+              		// Call to Wisita API to retrieve uploaded file
               		$ctrl.getMedia();
             	}
           	},
 	        progressall: function (e, data) {
-	        	if (data.total > 0) {
-		            $scope.$apply(function() {
-		            	$scope.progress = parseInt(data.loaded / data.total * 100, 10);
-		            });
-	            };
+	        	// Function to control progress bar
+	            $ctrl.progress = parseInt(data.loaded / data.total * 100, 10);
+	            $('#progress .progress-bar').css(
+	                'width',
+	                $ctrl.progress + '%'
+	            );
 	        }
 	    });
 	}
 
- angular.module('processApp')
-    .component('uploader', {
-  		templateUrl: 'uploader.html',
-  		controller: ['$scope', '$http', '$sce', '$timeout', UploadController],
-	});
-
 }());
-
-console.log("uploader.js");
